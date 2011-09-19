1f0a84dcb4f0ce10faad858d890ef8b16e935720
Leon Timmermans <fawaka@gmail.com>
2011-09-19 22:51:49 +0200

    Initial implementation

--- lib/Dist/Zilla/PluginBundle/LEONT/PP.pm
+++ lib/Dist/Zilla/PluginBundle/LEONT/PP.pm
@@ -1,5 +1,36 @@
 package Dist::Zilla::PluginBundle::LEONT::PP;
-use strict;
-use warnings;
+
+use Moose;
+use Dist::Zilla;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+my @bundles = qw/Basic LEONT::Base/;
+
+sub configure {
+	my $self = shift;
+
+	$self->add_bundle("\@$_") for @bundles;
+	return;
+}
 
 1;
+
+__END__
+
+#ABSTRACT: Plugins LeonT uses for pure perl modules
+
+=head1 DESCRIPTION
+
+This is identical to the following setup:
+
+    [@Basic]
+	[@LEONT::Base]
+
+=begin Pod::Coverage
+
+configure
+
+=end Pod::Coverage
+
+=cut
+
